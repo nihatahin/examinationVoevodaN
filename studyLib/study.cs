314df66952790bc8ce073d180b365091eb8431f2
nihatahin <threespots@mail.ru>
2022-08-07 19:23:38 +0300

    First README item was realized.

--- studyLib/study.cs
+++ studyLib/study.cs
@@ -1,5 +1,21 @@
 ﻿namespace studyLib;
 public class study
 {
-    
+    public static string[] getArrOfSmallString(string[] array, int strMaxSize)
+    {
+        int cnt = 0;
+        for (int i = 0; i < array.Length; ++i)
+        {
+            if (array[i].Length <= strMaxSize)
+            {
+                array[cnt] = array[i];
+                ++cnt;
+            }
+        }
+
+        Console.WriteLine(cnt);
+
+        return array;
+    }
+////////////////////////////////////////////////////////////////////////////////
 }
